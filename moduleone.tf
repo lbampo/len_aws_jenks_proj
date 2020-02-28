f61543ed66de1daa8030bcc041a1f9f93ccacb71
Lennox Bampoe-Addo <lennox@Lennoxs-MacBook-Pro.local>
2020-02-28 16:01:32 +0000

    testing changes

---
+++ moduleone.tf
@@ -0,0 +1,140 @@
+
+#--------------------------
+
+# PROVIDERS
+
+provider "aws" {
+    region = "eu-west-1"
+}
+#
+
+#----------------------------
+# RESOURCES
+
+resource "aws_instance" "jenkins" {
+    name = "len_nginx_1"
+    ami = "${var.ami_len_jenk}"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.len_subnet_1.id}"
+    vpc_security_group_ids = ["${aws_security_group.len_sg.id}"]
+    key_name = "len_terra"
+}
+
+resource "aws_instance" "len_nginx"{
+    name = "len_nginx_2"
+    ami = "ami-035966e8adab4aaad"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.len_subnet_2.id}"
+    vpc_security_group_ids = ["${aws_security_group.len_sg.id}"]
+    key_name = "len_terra"
+}
+
+resource "aws_key_pair" "deployer"{
+    key_name = "len_terra"
+    public_key = "${var.dontlook}"
+}
+
+
+
+
+
+
+#-------------------------------
+# NETWORKING
+
+resource "aws_vpc" "len_vpc"{
+    cidr_block = "${var.network_address_space}"
+    enable_dns_hostnames = "true"
+}
+
+resource "aws_internet_gateway" "len_igw"{
+    vpc_id = "${aws_vpc.len_vpc.id}"
+}
+
+resource "aws_subnet" "len_subnet_1"{
+    cidr_block = "${var.subnet_1}"
+    vpc_id = "${aws_vpc.len_vpc.id}"
+    map_public_ip_on_launch = "true"
+}
+
+resource "aws_subnet" "len_subnet_2"{
+    cidr_block = "${var.subnet_2}"
+    vpc_id = "${aws_vpc.len_vpc.id}"
+    map_public_ip_on_launch = "true"
+}
+
+# Routing 
+
+resource "aws_route_table" "rtb"{
+    vpc_id = "${aws_vpc.len_vpc.id}"
+
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id ="${aws_internet_gateway.len_igw.id}"
+    }
+}
+
+resource "aws_route_table_association" "rta_subnet1"{
+    subnet_id = "${aws_subnet.len_subnet_1.id}"
+    route_table_id = "${aws_route_table.rtb.id}"
+}
+
+resource "aws_route_table_association" "rta_subnet2"{
+    subnet_id = "${aws_subnet.len_subnet_2.id}"
+    route_table_id = "${aws_route_table.rtb.id}"
+
+}
+
+# Security Groups
+
+resource "aws_security_group" "len_sg"{
+    name = "len_sg"
+    vpc_id = "${aws_vpc.len_vpc.id}"
+
+
+# SSH
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# HTTP
+
+    ingress{
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# HTTPS
+
+    ingress{
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# Custom TCP Rule
+
+    ingress{
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# Egress 
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
---
+++ variables.tf
@@ -0,0 +1,25 @@
+
+
+variable "secret_key"{
+    default = "gs+hmnqxPyGVCBz7X0/lfInu6PPUaVkR/3Mcm4rF"
+}
+
+variable "ami_len_jenk"{
+    default = "ami-024efc2adc6a09ebd"
+}
+
+variable "network_address_space"{
+    default = "10.1.0.0/16"
+}
+
+variable "subnet_1"{
+    default = "10.1.0.0/24"
+}
+
+variable "subnet_2"{
+    default = "10.1.1.0/24"
+}
+
+variable "dontlook"{
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCz5GkX6bhbwq+aB7jWxrqZgTwQ5BxvunNlInOtlc6tskqutsoro7bUfujJg5cCyvvIundCD8anHMD3M1Md1h2oay/FME/4cirDWndjNJXRtBQVEIVkomVgHbvfckANiP6IkW0QtG38kjfasdaIkCrHgmBb7kZgnzdZAgOpECXsvsvYY48o+QDqmK5CBVHYJx6ZeLYvnTkXxda+LkGJl3gTbRtm0AshwpsHCvi2gsX5WX3Sqtacy0Li8X9k34DoQOpkZhEUeYCe07TBULAqck77euVgVfsR1vSzSnyxjsheZrEif6tP0DRlkFwW9aONp8xfA+sDSh7BsgvDHTKtAnGMnv0olbjHK+BK07HglvC4cJ9aCcKQRR4wAAldcjPZoqd/pv+2CzaiQwd7fhX+6g/6pP5ksTuZMpj3+4nGkcXp0tBKHTZif80wsCbLdQq3yjqQK1HToaUWvR4LHqi5GAKoVInPB88jBTt9C1d0wAFEQhFCDWvnoTlNRt1wp/L65SEy2ED34HmdI/DMlnDT0fYinK9cfqoO41kWtNZQBLv0TyJklrY0J7+JNLaqPoKCucV5GsPGHcCpX5tuYHXVYkt6akxk/VZ2ZfkLsSTDZo4nUvy3UqveDIhZI2bGgrt5/KnLS1RmZsenBQNChFtrZoQyhv+239wrVLSRlWO+B6soIQ== lennox_addo@yahoo.com"
+}
